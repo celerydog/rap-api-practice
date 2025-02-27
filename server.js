e5c3c67e7c9a51c0b6df07cbad31218f7a041e35
celerydog <lizzy.eden@gmail.com>
2025-02-27 17:01:43 +1300

    thanks jeff :)

--- server.js
+++ server.js
@@ -21,8 +21,28 @@ const rappers = {
     
 }
 
+
+/* 
+    1. browser asks for /
+    2. server sends contents of index.html
+    3. browser knows how to handle html, notices html is referencing a js 
+    4. browser asks for `/js/main.js`
+     (Assuming we fix bug: missing endpoint)
+    5. server sends back contents of `/js/main.js` 
+    6. browser knows how to handle js, runs code
+
+    ... (later on)
+
+    7. click the button, browser makes a request to /api/${rapperName} (fetch)
+    8. server matches request to endpoint `/api/:rappername`, does something with it
+*/
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/js/:scriptName', (req,res) => {
+    res.sendFile(__dirname + `/js/${req.params.scriptName}`)
 })
 
 app.get('/api/:rapperName', (req,res) => {
